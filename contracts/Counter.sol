7983cc057ce20f2b3f133871f7c4f336361d2df9
amitg01 <amitg.ghy@gmail.com>
2023-11-19 21:59:49 +0530

    added get name method

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -26,6 +26,10 @@ contract Counter {
         number = _num;
     }
 
+    function setName(string memory _name) public {
+      name = _name;
+    }
+
     function getName() public view returns(string memory){
       return name;
     }
